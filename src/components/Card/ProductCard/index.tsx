3fdf180c9dd4135f3b3edbcf9b1b2adc79c52bbd
hamidrezanikoonia <hamid.nikoonia@ecashpay.com.ph>
2024-12-01 01:50:34 +0330

    fixissue minor changes

--- src/components/Card/ProductCard/index.tsx
+++ src/components/Card/ProductCard/index.tsx
@@ -3,6 +3,11 @@ import { Star, ShoppingCart } from 'lucide-react';
 
 import {IProduct} from '@/types/Product';
 
+
+const calculateDiscountByPercentage = (currentPrice: number, discountPercentage: number): number => {
+  return  currentPrice * ((100 - discountPercentage) / 100);
+}
+
 // interface Product {
 //   id: number
 //   subtitle: string
@@ -41,10 +46,13 @@ export default function ProductCard({product}: {product: IProduct}) {
   return (
 <div className="relative w-full max-w-xs overflow-hidden rounded-lg bg-white shadow-md">
   <a href="#">
-    <img className="h-60 rounded-t-lg object-cover" src={`${NEXT_PUBLIC_SERVER_FILES_URL}/${product?.thumbnail.file_name}`} alt={product.title} />
+     <img className="h-60 rounded-t-lg object-cover" src={product?.thumbnail?.file_name ? `${NEXT_PUBLIC_SERVER_FILES_URL}/${product?.thumbnail?.file_name}` : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC5F52ynaRIN577hgyivShpnSw6iHDH_dDAg&s'} alt={product.title} />
   </a>
   {/* If product not exist */}
   {!product?.is_available && <span className="absolute top-0 left-0 w-28 translate-y-4 -translate-x-6 -rotate-45 bg-black text-center text-xs text-white">ناموجود</span>}
+  {(product?.is_available && product?.discountable?.status) && <span className="absolute top-0 left-0 w-28 translate-y-4 -translate-x-6 -rotate-45 bg-red-500 text-center text-xs text-white">
+    %{product?.discountable.percent.toLocaleString('ar-EG')} OFF
+    </span>}
   
   <div className="mt-4 px-5 pb-5">
     <a href="#">
@@ -70,17 +78,17 @@ export default function ProductCard({product}: {product: IProduct}) {
     <div className="flex items-center justify-between">
       <div>
         <div className="flex text-lg font-bold items-center text-slate-900">
-        <span className='mr-1 text-sm'>تومان</span>
-          <span>{product.price.toLocaleString('ar-EG')}</span>
+        {product?.is_available && <span className='mr-1 text-sm'>تومان</span>}
+        {product?.is_available ? (<span>{product.price.toLocaleString('ar-EG')}</span>) : 'ناموجود'} 
         </div>
-        {product?.discountable?.status && (<span className=" inline-block text-sm text-slate-900 line-through">
-            تخفیف %{product?.discountable.percent.toLocaleString('ar-EG')}
+        {(product?.discountable?.status && product?.is_available) && (<span className=" inline-block text-sm text-slate-900 line-through">
+          {calculateDiscountByPercentage(product.price, product?.discountable.percent).toLocaleString('ar-EG')}
         </span>)}
         
       </div>
-      <a href="#" className="flex items-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+      {product?.is_available && (<a href="#" aria-disabled className="flex items-center rounded-md disabled:opacity-5 bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
         <ShoppingCart className='mr-2' size={19} />
-         خرید</a>
+         خرید</a>)}
     </div>
   </div>
 </div>
